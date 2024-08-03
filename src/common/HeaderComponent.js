0db248f7c03bccd5ccac12ae5f5ed9add5db9dfc
Vinay Kumar Mareedu <krishnachaitanya@techouts.com>
2024-08-03 15:57:06 +0530

    header component updated

--- src/common/HeaderComponent.js
+++ src/common/HeaderComponent.js
@@ -4,7 +4,7 @@ import {
 const Header =()=> {
     return (
         <header className="header">
-            <RightCircleOutlined style={{ fontSize: '26px', color: '#08c' }}/>
+            <RightCircleOutlined style={{ fontSize: '2git 6px', color: '#08c' }}/>
         </header>
     );
 }
